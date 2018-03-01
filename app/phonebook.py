5067298a0db2d975a843132781c75181ffdc4634
naibor <liznaibor.en@gmail.com>
2018-03-01 13:23:16 +0300

    first commit

---
+++ app/phonebook.py
@@ -0,0 +1,11 @@
+class Contact (object):
+	"""docstring for Contact """
+	def __init__(self, name,number):
+		self.name =name
+		self.number=number
+
+contactone=Contact(liz,456469879)
+print (contactone.name)
+.
+
+
---
+++ test/test_phonebook.py
@@ -0,0 +1,8 @@
+import unittest
+
+class testContact(unittest.TestCase):
+	"""docstring for testContact"""
+	def test_phonebook_add_method_return_correct_result(self):
+		
+
+
